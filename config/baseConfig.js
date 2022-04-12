a64fecb177c7d1f66c66e8cf9e4cff3577af56ef
AmandaYi（891177434@qq.com） <891177434@qq.com>
2022-04-12 17:52:34 +0800

    数据结构与算法分析

--- config/baseConfig.js
+++ config/baseConfig.js
@@ -1,4 +1,4 @@
-
+/** @type {import('@docusaurus/types').Config} */
 const baseConfig = {
 	title: '无缘无故~望着我',
 	tagline: '敛冷于眼，藏拙于神，潜动于灵魂，似愚非愚，大巧无巧，我们在思考中抵达内心的宁静和丰富。吾本来兹土,传法救迷情。一花开五叶，结果自然成。​',
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,23 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const navbarConfig = require("./config/navbarConfig");
 const footerConfig = require("./config/footerConfig");
 const baseConfig = require("./config/baseConfig")
+let scripts = []
+// 代码统计功能
+if(process.env.NODE_ENV  === "production") {
+	scripts.push({
+		src: 'https://hm.baidu.com/hm.js?0c2f44724c29f9207682097a4ae0ee1d',
+		async: true,
+	})
+}
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	...baseConfig,
+	title: baseConfig.title,
+	tagline: baseConfig.tagline,
+	url: baseConfig.url,
+	baseUrl: baseConfig.baseUrl,
+	onBrokenLinks: baseConfig.onBrokenLinks,
+	onBrokenMarkdownLinks: baseConfig.onBrokenMarkdownLinks,
+	scripts: [...scripts] ,
 	// favicon: 'img/favicon.ico',
 	// organizationName: 'facebook', // Usually your GitHub org/user name.
 	// projectName: 'docusaurus', // Usually your repo name.
@@ -32,7 +46,7 @@ const config = {
 					blogSidebarCount: 'ALL',
 					// Please change this to your repo.
 					// editUrl:
-						// 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+					// 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
@@ -61,6 +75,39 @@ const config = {
 				darkTheme: darkCodeTheme,
 			},
 		}),
+
+
+	ssrTemplate: `<!DOCTYPE html>
+<html <%~ it.htmlAttributes %>>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="generator" content="Docusaurus v<%= it.version %>">
+    <% if (it.noIndex) { %>
+      <meta name="robots" content="noindex, nofollow" />
+    <% } %>
+    <%~ it.headTags %>
+    <% it.metaAttributes.forEach((metaAttribute) => { %>
+      <%~ metaAttribute %>
+    <% }); %>
+    <% it.stylesheets.forEach((stylesheet) => { %>
+      <link rel="stylesheet" href="<%= it.baseUrl %><%= stylesheet %>" />
+    <% }); %>
+    <% it.scripts.forEach((script) => { %>
+      <link rel="preload" href="<%= it.baseUrl %><%= script %>" as="script">
+    <% }); %>
+  </head>
+  <body <%~ it.bodyAttributes %>>
+    <%~ it.preBodyTags %>
+    <div id="__docusaurus">
+      <%~ it.appHtml %>
+    </div>
+    <% it.scripts.forEach((script) => { %>
+      <script src="<%= it.baseUrl %><%= script %>"></script>
+    <% }); %>
+    <%~ it.postBodyTags %>
+  </body>
+</html>`,
 };
 
 module.exports = config;
